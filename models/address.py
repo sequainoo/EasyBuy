379c17c0e9b1db6ab72c70e8bb84a6bbc1e6fbd8
sequainoo <wazatoyung@gmail.com>
2022-05-11 20:08:32 +0000

    Add Address Model framework

---
+++ models/address.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines the  Address model"""
+
+from models.base import BaseModel
+
+
+class Address(BaseModel):
+    """Address model definition.
+
+    Attributes:
+        user_id (str): user with that address
+        Region_id (str): Region located such as Ashanti, Greater Accra.
+        city_id (str): city with the region
+        Town (str): Town of the address
+        phone_number (str): ten or 13(with code prefix and + sign) digit phone number
+    """
+    user_id = ''
+    region_id = ''
+    city_id = ''
+    town = ''
+    phone_number = ''
