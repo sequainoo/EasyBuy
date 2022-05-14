1382b8737e141367f8fb365df2eb324c8c183961
sequainoo <wazatoyung@gmail.com>
2022-05-14 22:06:15 +0000

    Add imports the initialize all views on the app_views Blueprint

--- web_app/views/__init__.py
+++ web_app/views/__init__.py
@@ -3,3 +3,9 @@
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__)
+
+from web_app.product_list_view import *
+from web_app.product_detail_view import *
+from web_app.payment_view import *
+from web_app.checkout_view import *
+from web_app.cart_detail_view import *
