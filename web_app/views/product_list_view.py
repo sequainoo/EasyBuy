795dfd84cbc7ee517bb6c5a390cc893d3b7f7028
sequainoo <wazatoyung@gmail.com>
2022-05-14 15:27:46 +0000

    Change product_list_view route from / to /products

--- web_app/views/product_list_view.py
+++ web_app/views/product_list_view.py
@@ -7,10 +7,10 @@ from web_app.views import app_views
 from models import storage
 
 
-@app_views.route('/', strict_slashes=False)
+@app_views.route('/products', strict_slashes=False)
 def product_list_view():
-    """Returns the home page populated with a list of products"""
+    """Returns the products page populated with a list of products"""
     products = storage.all('Product')
     brands = storage.all('Brand')
 
-    return render_template('home.html', products=products, brands=brands)
+    return render_template('product_list.html', products=products, brands=brands)
