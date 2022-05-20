ae1bbb1c20d5059f7ed6d595f3313f577c2d589b
sequainoo <wazatoyung@gmail.com>
2022-05-20 02:30:46 +0000

    Add valid_image_filename utitlity

--- utilities/small_helpers.py
+++ utilities/small_helpers.py
@@ -7,3 +7,10 @@ import uuid
 def uuid4():
     """Returns a string representation of uuid4"""
     return str(uuid.uuid4())
+
+
+def valid_image_name(filename):
+    """checks if a filename has an image kind extension"""
+    return '.' in filename\
+        and filename.rsplit('.', 1)[1].lower()\
+        in ['png', 'gif', 'jpeg', 'jpg', 'webp']
