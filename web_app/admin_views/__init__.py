b7edacb48718214c9e9ef957bb087899c8ab018d
sequainoo <wazatoyung@gmail.com>
2022-05-20 02:05:56 +0000

    Import all admin views modules in the admin_views __init__.py

--- web_app/admin_views/__init__.py
+++ web_app/admin_views/__init__.py
@@ -2,4 +2,12 @@
 
 from flask import Blueprint
 
-admin_views = Blueprint('admin_views', __name__)
+admin_views = Blueprint('admin_views', __name__, url_prefix='/admin')
+
+from web_app.admin_views.brand_create_view import *
+from web_app.admin_views.image_upload_view import *
+from web_app.admin_views.phone_create_view import *
+from web_app.admin_views.order_list_view import *
+from web_app.admin_views.process_order_view import *
+from web_app.admin_views.phone_list_view import *
+from web_app.admin_views.ship_order_view import *
