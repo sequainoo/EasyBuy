cce674714162725168a50d7fb67b703ed4a28ec8
sequainoo <wazatoyung@gmail.com>
2022-06-01 17:05:02 +0000

    rm startupd crpt

--- web_app/app.py
+++ web_app/app.py
@@ -13,6 +13,6 @@ app.register_blueprint(app_views)
 app.register_blueprint(admin_views)
 app.secret_key = b'easybuy'
 
-cors = CORS(app, resources={r'/*': {'origins': '*', 'methods': '*', 'headers': ['Content-Type']}})
+cors = CORS(app, resources={r'/*': {'origins': '*', 'methods': '*'}})
 if __name__ == '__main__':
     app.run()
