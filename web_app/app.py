2e40d145bea7c8af66afa3bc35e5998a705674cd
sequainoo <wazatoyung@gmail.com>
2022-06-01 16:41:55 +0000

    remove cors headers content-type

--- web_app/app.py
+++ web_app/app.py
@@ -9,10 +9,11 @@ from .admin_views import admin_views
 
 
 app = Flask(__name__)
-app.config['CORS_HEADERS'] = 'Content-Type'
 app.register_blueprint(app_views)
 app.register_blueprint(admin_views)
-cors = CORS(app, resources={r'/*': {'origins': '*', 'methods': '*', 'headers': '*'}})
 app.secret_key = b'easybuy'
+
+cors = CORS(app, resources={r'/*': {'origins': '*', 'methods': '*'}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 if __name__ == '__main__':
     app.run()
