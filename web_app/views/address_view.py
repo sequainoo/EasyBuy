1209a1dd43bcd2d05cd3da9c5832460130f0defb
sequainoo <wazatoyung@gmail.com>
2022-06-03 03:27:00 +0000

    Refactor address_view

--- web_app/views/address_view.py
+++ web_app/views/address_view.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Payment view Definition"""
+"""Defines view to create customer address"""
 
 import json
 from datetime import date, datetime, timedelta
@@ -13,9 +13,9 @@ from models import storage, Address
 
 @app_views.route('/address', methods=['POST'], strict_slashes=False)
 def address_view():
-    """Process address
+    """Creates address for a customer
 
-    Post data format:
+    Post address_info format:
         {
             "customer_id": "<customer_id>",
             "region_id": <region_id>,
@@ -23,63 +23,36 @@ def address_view():
             "town": <town>,
             "phone_number": <phone_number>
         }
+    id of order is passed as querystring to return to checkout page of the
+    specific order that address creation request came from.
+    ?order_id=id
 
     Return:
-        if successful return a succes page
-        else unsuccessful page
+        checkout page with either success or failed status message
     """
-    # retrieve all address info
-    data = request.form
+    # retrieve all address information from the form
+    address_info = request.form
 
-    customer_id = data.get('customer_id', None)
-    region_id = data.get('region_id', None)
-    city_id = data.get('city_id', None)
-    town = data.get('town', None)
-    phone_number = data.get('phone_number', None)
+    customer_id = address_info.get('customer_id')
+    region_id = address_info.get('region_id')
+    city_id = address_info.get('city_id')
+    town = address_info.get('town')
+    phone_number = address_info.get('phone_number')
     
-    # make sure information is right
-    if not customer_id or not region_id or not city_id\
-        or not town or not phone_number:
-        flash('incomplete form')
+    # if validation of address info fails go back to the checkout page
+    if not validate_address_info(address_info):
         return redirect(
             url_for('app_views.checkout_existing_order_view',
             order_id=request.args['order_id'])
         )
 
+    # set old addresses of the customer to not default while new is created
     customer = storage.get('customer', customer_id)
-    if not customer:
-        flash('Customer not exist')
-        return redirect(
-            url_for('app_views.checkout_existing_order_view',
-            order_id=request.args['order_id'])
-        )
-
-    if not storage.get('region', region_id):
-        flash('Region not exist')
-        return redirect(
-            url_for('app_views.checkout_existing_order_view',
-            order_id=request.args['order_id'])
-        )
-
-    if not storage.get('city', city_id):
-        flash('City not exist')
-        return redirect(
-            url_for('app_views.checkout_existing_order_view',
-            order_id=request.args['order_id'])
-        )
-
-    if len(phone_number) < 10 or len(phone_number) > 13:
-        flash('Phone not valid')
-        return redirect(
-            url_for('app_views.checkout_existing_order_view',
-            order_id=request.args['order_id'])
-        )
-
-    # set all existing addresses to not default while new is created
     if len(customer.addresses) > 0:
         for address in customer.addresses:
             address.default = False
 
+    # create address object from the valid info
     address = Address(customer_id=customer_id,
                       region_id=region_id,
                       city_id=city_id,
@@ -96,9 +69,43 @@ def address_view():
             url_for('app_views.checkout_existing_order_view',
             order_id=request.args['order_id'])
         )
-    # return jsonify({'address_id': address.id})
     flash('Successfully added')
     return redirect(
         url_for('app_views.checkout_existing_order_view',
         order_id=request.args['order_id'])
     )
+
+
+def validate_address_info(address_info):
+    """Validates address  information passed from the form."""
+    customer_id = address_info.get('customer_id', None)
+    region_id = address_info.get('region_id', None)
+    city_id = address_info.get('city_id', None)
+    town = address_info.get('town', None)
+    phone_number = address_info.get('phone_number', None)
+    
+    # make sure information is right
+    if not customer_id or not region_id or not city_id\
+        or not town or not phone_number:
+        flash('incomplete form')
+        return False
+
+    customer = storage.get('customer', customer_id)
+    if not customer:
+        flash('Customer not exist')
+        return False
+
+    if not storage.get('region', region_id):
+        flash('Region not exist')
+        return False
+
+    if not storage.get('city', city_id):
+        flash('City not exist')
+        return False
+
+    # extra checks for a valid phone would be added
+    if len(phone_number) < 10 or len(phone_number) > 13:
+        flash('Phone not valid')
+        return False
+
+    return True
--- web_app/views/cart_detail_view.py
+++ web_app/views/cart_detail_view.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Cart view Definition"""
+"""Defines a view to generate a page for a cart"""
 
 from flask import render_template, request, redirect, url_for
 
@@ -12,24 +12,38 @@ from utilities.small_helpers import uuid4
 def cart_detail_view():
     """Returns a Cart page.
 
-    As Query String:
-        /cart?id=qty&id1=qty..
+    Expects Query String in the format /cart?id=qty&id1=qty..:
+        id (str): id of phone to purchase.
+        qty (int): quantity of the phone.
 
-    Renders:
+    Return:
         The cart page with either None as cart data or
         the cart data if it was sent in a format specified as above
     """
     cart = request.args
 
+    # if no cart data is passed return to the phone list page
     if not cart:
         return redirect(url_for('app_views.phone_list_view'))
+
+    # iterate over each phone id and quantity pair,
+    # obtain phone object from database with id and
+    # build phones_quantities list as a tuple of phone object
+    # and quantity.
+    # Check for bad data and when found return to phones page
+
     phones_quantities = []
     for id, qty in cart.items():
         try:
             qty = int(qty)
-            phones_quantities.append((storage.get('phone', id), qty))
+            phone = storage.get('phone', id)
+            if not phone:
+                raise ValueError
+            phones_quantities.append(phone, qty)
         except (ValueError, TypeError):
             return redirect(url_for('app_views.phone_list_view'))
+
+    # render cart page with phones_quantities list
     return render_template('cart.html',
                            phones_quantities=phones_quantities,
                            id=uuid4())
